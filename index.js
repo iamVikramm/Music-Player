3c4010a11f613804b115e15db6aa2996290027c2
Vikram <115387149+iamVikramm@users.noreply.github.com>
2022-12-02 09:09:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+for(var i=0; i<document.querySelectorAll(".art-image").length; i++){
+let photos=document.querySelectorAll(".art-image")[i].addEventListener("click", redirect);
+}
+
+function redirect(){
+  window.location="file:///C:/Users/saivi/OneDrive/Desktop/WebDevelopment/MyProjects/Ninja%20Studio/SinglePlaylist.html";
+}
